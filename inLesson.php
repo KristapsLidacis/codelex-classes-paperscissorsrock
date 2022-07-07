8a978eecd18d4e9d9f86fcc78917c2a484e94484
Kristaps <lidacisk@gmail.com>
2022-07-07 10:16:12 +0300

    Added Tournament class (my interpretation) and added 05072022 lessons teachers code

--- inLesson.php
+++ inLesson.php
@@ -41,160 +41,205 @@ class Player{
     {
         return $this->hand;
     }
-    public function getIsCPU():bool{
+    public function isCPU(): bool
+    {
         return $this->isCPU;
     }
+
 }
-class GamePlay{
+class Game{
+    /** @var Element[] */
     private array $elements =[];
-    public function __construct()
+    private ?Player $winner = null;
+    private Player $attacker;
+    private Player $defender;
+    public function __construct(Player $attacker, Player $defender)
     {
+        $this->attacker=$attacker;
+        $this->defender=$defender;
         $this->setup();
-    }
-    public function start(Player $p1, Player $p2, int $p1Index, int $p2Index):?Player{
-        $p11 =$p1;
-        $p21 =$p2;
-        $p11->setHand($this->elements[$p1Index]);
-        $p21->setHand($this->elements[$p2Index]);
-        echo $p11->getPlayerName()." uses ".$p11->getHand()->getElementName()." against ". $p21->getPlayerName()."'s "
-            .$p21->getHand()->getElementName().PHP_EOL;
-        if($p11->getHand() === $p21->getHand()){
-            return null;
-        }
-        if($p21->getHand()->winsAgainst($p11->getHand())){
-            return $p21;
-        }
-        if($p11->getHand()->winsAgainst($p21->getHand())){
-            return $p11;
-        }
-        return null;
     }
     public function setup():void{
         $this->elements=[
             $rock = new Element('Rock'),
             $paper = new Element('Paper'),
             $scissors = new Element('Scissors'),
-            $lizard = new Element('Lizard'),
-            $spock = new Element('Spock'),
         ];
-        $rock->addWinnings([$scissors, $lizard]);
-        $paper->addWinnings([$rock, $spock]);
-        $scissors->addWinnings([$paper, $lizard]);
-        $lizard->addWinnings([$paper, $spock]);
-        $spock->addWinnings([$scissors, $rock]);
-
-    }
-    public function displayElements():void{
-        foreach ($this->elements as $keys => $element) {
-            echo "[{$keys}] - {$element->getElementName()}".PHP_EOL;
-        }
+        $rock->addWinnings([$scissors]);
+        $paper->addWinnings([$rock]);
+        $scissors->addWinnings([$paper]);
     }
     public function getElements():array{
         return $this->elements;
     }
-
-}
-class GameTournament{
-    private array $score=[];
-    private array $players=[];
-    private GamePlay $game;
-    public function __construct()
+    public function determineResult():void
     {
-        $this->setup();
-        $this->game = new GamePlay();
+        if($this->attacker->getHand() === $this->defender->getHand()){
+            $this->winner=null;
+        }
+        if($this->attacker->getHand()->winsAgainst($this->defender->getHand())){
+            $this->winner=$this->defender;
+         //   var_dump($this->attacker->getHand()->getElementName(), $this->defender->getHand()->getElementName());
+        }
+        if($this->defender->getHand()->winsAgainst($this->attacker->getHand())){
+            $this->winner=$this->attacker;
+        }
+    }
+    public function isTied():bool
+    {
+        return is_null($this->winner);
+    }
+    public function getWinner(): ?Player
+    {
+        return $this->winner;
+    }
+}
+class GameSet{
+    private Player $attacker;
+    private Player $defender;
+    private Player $winner;
+    private array $games=[];
+    private const MAX_WINS=2;
+    private int $attackerPoints=0;
+    private int $defenderPoint=0;
+    public function __construct(Player $attacker, Player $defender)
+    {
+        $this->attacker=$attacker;
+        $this->defender=$defender;
     }
 
-    public function startTournament():void
+    public function determineResult(): void
     {
-        $winnerFound = false;
-        while(!$winnerFound){
-            echo count($this->players).PHP_EOL;
-            $p1Index=$this->getIndex();
-            $p2Index=$this->getIndex();
-            if($p1Index != $p2Index){
-                $p1Score =0;
-                $p2Score =0;
-                $bo3=false;
-                echo  '---------------------------'.PHP_EOL;
-                if($this->players[$p1Index]->getIsCPU()&&$this->players[$p2Index]->getIsCPU()){
-                    while(!$bo3){
-                        $p1MoveIndex = rand(0, count($this->game->getElements())-1);
-                        $p2MoveIndex = rand(0, count($this->game->getElements())-1);
-                        $winner = $this->game->start($this->players[$p1Index], $this->players[$p2Index], $p1MoveIndex, $p2MoveIndex);
-                        if($winner === $this->players[$p1Index]){
-                            $p1Score++;
-                        }elseif($winner=== $this->players[$p2Index]){
-                            $p2Score++;
-                        }
-                        echo $winner != null ? $winner->getPlayerName()." has won".PHP_EOL:"It's a tie".PHP_EOL;
-
-                        if($p1Score >= 2 || $p2Score >= 2){
-                            $bo3 = true;
-                        }
-                    }
-                }else{
-                    while(!$bo3) {
-                        $this->game->displayElements();
-                        $playerMoveIndex = (int)readLine('Choose type: ');
-                        $playerIndex = $this->players[$p1Index]->getIsCPU() ? $this->players[$p2Index] : $this->players[$p1Index];
-                        $cpu = $this->players[$p1Index]->getIsCPU() ? $this->players[$p1Index] : $this->players[$p2Index];
-                        $cpuMoveIndex = rand(0, count($this->game->getElements()) - 1);
-                        $winner = $this->game->start($cpu, $playerIndex, $cpuMoveIndex, $playerMoveIndex);
-                        if ($winner === $cpu) {
-                            $p1Score++;
-                        } elseif ($winner === $playerIndex) {
-                            $p2Score++;
-                        }
-                        echo $winner != null ? $winner->getPlayerName()." has won" . PHP_EOL : "It's a tie" . PHP_EOL;
-
-                        if ($p1Score >= 2 || $p2Score >= 2) {
-                            $bo3 = true;
-                        }
-                    }
-                }
-                if($p1Score >= 2){
-                    $this->score[] = $this->players[$p2Index];
-                    echo $this->players[$p2Index]->getPlayerName().' has been eliminated'.PHP_EOL;
-                    array_splice($this->players, $p2Index, 1);
-                }
-                if($p2Score >= 2){
-                    $this->score[] = $this->players[$p1Index];
-                    echo $this->players[$p1Index]->getPlayerName().' has been eliminated'.PHP_EOL;
-                    array_splice($this->players, $p1Index, 1);
-                }
-                echo  '---------------------------'.PHP_EOL;
-            }else{
+        while($this->attackerPoints < self::MAX_WINS && $this->defenderPoint < self::MAX_WINS){
+            $game = new Game($this->attacker, $this->defender);
+            $this->games[] = $game;
+            $elements = $game->getElements();
+            $attackerSelectedIndex = array_rand($elements);
+            $defenderSelectedIndex = array_rand($elements);
+            $this->attacker->setHand($elements[$attackerSelectedIndex]);
+            $this->defender->setHand($elements[$defenderSelectedIndex]);
+            $game->determineResult();
+            //var_dump($this->attackerPoints, $this->defenderPoint);
+            if($game->isTied()){
                 continue;
             }
-
-            if(count($this->players) === 1){
-                $this->score[] = $this->players[0];
-                $winnerFound=true;
+            if($game->getWinner()===$this->attacker){
+                $this->attackerPoints++;
+            }
+            if($game->getWinner()===$this->defender){
+                $this->defenderPoint++;
             }
         }
-        $reverseScore = array_reverse($this->score);
-        $key= 1;
-        foreach($reverseScore as $sco){
-            echo "|". $key ." place - ".$sco->getPlayerName();
-            $key++;
+        if($this->attackerPoints > $this->defenderPoint){
+            $this->winner=$this->attacker;
+            return;
         }
+
+        $this->winner=$this->defender;
+
     }
-    public function getIndex():int{
-        return rand(0, count($this->players)-1);
-    }
-    private function setup():void
+    public function getAttacker(): Player
     {
-        for($i = 0; $i <= 8; $i++){
-            if($i==0){
-                $this->players[]= new Player('Player', false);
-
-            }
-            $this->players[] = new Player("CPU$i", true);
-        }
-
+        return $this->attacker;
     }
-
+    public function getDefender(): Player
+    {
+        return $this->defender;
+    }
+    public function getWinner(): Player
+    {
+        return $this->winner;
+    }
+    public function getAttackerPoints(): int
+    {
+        return $this->attackerPoints;
+    }
+    public function getDefenderPoint(): int
+    {
+        return $this->defenderPoint;
+    }
+    public function getGames(): array
+    {
+        return $this->games;
+    }
+}
+class Tournament{
+    ///Functions like => determineResult
+    ///Function => setPlayers, getPlayers
+    ///Function => setPlaces, getPlaces (array_unshift)
+    private array $players;
+    private array $winner = [];
+    public function addPlayer(Player $player): void{
+        $this->players[] = $player;
+    }
+    public function addPlayers(array $players): void{
+        foreach ($players as $player){
+            if(!$player instanceof Player) continue;
+            $this->addPlayer($player);
+        }
+    }
+    public function determineResult(Player $p1, Player $p2): void{
+        $set = new GameSet($p1, $p2);
+        $set->determineResult();
+        array_unshift($this->winner, $set->getWinner());
+    }
+    public function getPlayers(): array
+    {
+        return $this->players;
+    }
+    public function getWinner(): array
+    {
+        return $this->winner;
+    }
 }
 
-$gm = new GameTournament();
-$gm->startTournament();
+$t = new Tournament();
+$t->addPlayers([new Player('CPU1', true), new Player('CPU2', true), new Player('CPU3', true), new Player('CPU4', true),new Player('CPU5', true),new Player('CPU6', true),new Player('CPU7', true),new Player('CPU8', true),]);
+$t->determineResult($t->getPlayers()[0], $t->getPlayers()[1]);
+$t->determineResult($t->getPlayers()[2], $t->getPlayers()[3]);
+$t->determineResult($t->getPlayers()[4], $t->getPlayers()[5]);
+$t->determineResult($t->getPlayers()[6], $t->getPlayers()[7]);
+$winner = $t->getWinner();
+$t->determineResult($winner[3], $winner[2]);
+$t->determineResult($winner[2], $winner[1]);
+$t->determineResult($winner[1], $winner[0]);
+
+
+foreach ($t->getWinner() as $key => $player) {
+    echo $key == 0? "[$key] {$player->getPlayerName()}".PHP_EOL:"[$key] {$player->getPlayerName()}".PHP_EOL;
+}
+//$gameSet1 = new GameSet(new Player('CPU1'), new Player('CPU2'));
+//$gameSet1->determineResult();
+//$gameSet2 = new GameSet(new Player('CPU3'), new Player('CPU4'));
+//$gameSet2->determineResult();
+//$gameSet3 = new GameSet(new Player('CPU5'), new Player('CPU6'));
+//$gameSet3->determineResult();
+//$gameSet4 = new GameSet(new Player('CPU7'), new Player('CPU8'));
+//$gameSet4->determineResult();
+//$gameSet5 = new GameSet($gameSet1->getWinner(), $gameSet2->getWinner());
+//$gameSet5->determineResult();
+//$gameSet6 = new GameSet($gameSet3->getWinner(), $gameSet4->getWinner());
+//$gameSet6->determineResult();
+//$gameSet7 = new GameSet($gameSet5->getWinner(), $gameSet6->getWinner());
+//$gameSet7->determineResult();
+//echo "---------- quater -------------".PHP_EOL;
+//echo "{$gameSet1->getAttacker()->getPlayerName()}({$gameSet1->getAttackerPoints()}) vs {$gameSet1->getDefender()->getPlayerName()}({$gameSet1->getDefenderPoint()}) | Winner {$gameSet1->getWinner()->getPlayerName()}".PHP_EOL;
+//printGames($gameSet1);
+//echo "Winner is {$gameSet2->getAttacker()->getPlayerName()}({$gameSet2->getAttackerPoints()}) vs {$gameSet2->getDefender()->getPlayerName()}({$gameSet2->getDefenderPoint()}) | Winner {$gameSet2->getWinner()->getPlayerName()}".PHP_EOL;
+//echo "Winner is {$gameSet3->getAttacker()->getPlayerName()}({$gameSet3->getAttackerPoints()}) vs {$gameSet3->getDefender()->getPlayerName()}({$gameSet3->getDefenderPoint()}) | Winner {$gameSet3->getWinner()->getPlayerName()}".PHP_EOL;
+//echo "Winner is {$gameSet4->getAttacker()->getPlayerName()}({$gameSet4->getAttackerPoints()}) vs {$gameSet4->getDefender()->getPlayerName()}({$gameSet4->getDefenderPoint()}) | Winner {$gameSet4->getWinner()->getPlayerName()}".PHP_EOL;
+//echo "---------- semi -------------".PHP_EOL;
+//echo "Winner is {$gameSet5->getAttacker()->getPlayerName()}({$gameSet5->getAttackerPoints()}) vs {$gameSet5->getDefender()->getPlayerName()}({$gameSet5->getDefenderPoint()}) | Winner {$gameSet5->getWinner()->getPlayerName()}".PHP_EOL;
+//echo "Winner is {$gameSet6->getAttacker()->getPlayerName()}({$gameSet6->getAttackerPoints()}) vs {$gameSet6->getDefender()->getPlayerName()}({$gameSet6->getDefenderPoint()}) | Winner {$gameSet6->getWinner()->getPlayerName()}".PHP_EOL;
+//
+//echo "---------- final -------------".PHP_EOL;
+//echo "Winner is {$gameSet7->getAttacker()->getPlayerName()}({$gameSet7->getAttackerPoints()}) vs {$gameSet7->getDefender()->getPlayerName()}({$gameSet7->getDefenderPoint()}) | Winner {$gameSet7->getWinner()->getPlayerName()}".PHP_EOL;
+//
+//printGames($gameSet7);
+//
+//function printGames(GameSet $gameSetGames):void{
+//    foreach ($gameSetGames->getGames() as $index => $game) {
+//        $number = $index+1;
+//        echo $game->isTied()? "[$number] It's a tie".PHP_EOL:"[$number] {$game->getWinner()->getPlayerName()}".PHP_EOL;
+//    }
+//}
